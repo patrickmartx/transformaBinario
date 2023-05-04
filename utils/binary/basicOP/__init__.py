d3dd7b341acdd03a8baff28137d079d3f9a61ee9
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-05-04 12:34:50 -0300

    Função para operações básicas em binário de soma e subtração.

---
+++ utils/binary/basicOP/__init__.py
@@ -0,0 +1,63 @@
+def binSum(val1, val2, base=32):
+    val1 = val1[::-1]
+    val2 = val2[::-1]
+    valResult = ''
+    carry = False
+    for i in range(0, base):
+        if carry:
+            if val1[i] == val2[i] == "0":
+                valResult += "1"
+                carry = False
+            elif (val1[i] == "1" and val2[i] == "0") or (val1[i] == "0" and val2[i] == "1"):
+                valResult += "0"
+                carry = True
+            else:
+                valResult += "1"
+                carry = True
+        else:
+            if val1[i] == val2[i] == "0":
+                valResult += "0"
+                carry = False
+            elif (val1[i] == "1" and val2[i] == "0") or (val1[i] == "0" and val2[i] == "1"):
+                valResult += "1"
+                carry = False
+            else:
+                valResult += "0"
+                carry = True
+    return valResult[::-1]
+
+
+def binSubtract(val1, val2, base=32):
+    val1 = val1[::-1]
+    val2 = val2[::-1]
+    valResult = ''
+    carry = False
+    for i in range(0, base):
+        if carry:
+            if val1[i] == val2[i] == "0":
+                valResult += "0"
+                carry = True
+            elif val1[i] == val2[i] == "1":
+                valResult += "0"
+                carry = True
+            elif val1[i] == "1" and val2[i] == "0":
+                valResult += "0"
+                carry = False
+            else:
+                valResult += "0"
+                carry = True
+        else:
+            if val1[i] == val2[i] == "0":
+                valResult += "0"
+                carry = False
+            elif val1[i] == val2[i] == "1":
+                valResult += "0"
+                carry = True
+            elif val1[i] == "1" and val2[i] == "0":
+                valResult += "1"
+                carry = False
+            else:
+                valResult += "0"
+                carry = True
+    return valResult[::-1]
+
